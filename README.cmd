b17579811cda140f77e667fee08251ba4bc4a4d8
Benson Kamau <77424700+sirben181@users.noreply.github.com>
2021-05-07 23:04:03 +0300

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,1 +1,2 @@
-this is all about css grid, flexbox and nested
+i want to create a sample website by trying to duplicate an already existing website. this will basically use html,css and minimal js
+
